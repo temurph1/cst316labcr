092bbb68e4008e1cdae4f6b916ee189c6a4b0a54
Tommy <temurph1@asu.edu>
2016-02-22 00:01:28 -0700

    Added method signatures to AccountServerFactory

--- src/banking/primitive/core/AccountServerFactory.java
+++ src/banking/primitive/core/AccountServerFactory.java
@@ -11,10 +11,15 @@ public class AccountServerFactory {
 
 	protected static AccountServerFactory singleton = null;
 
+	/** Empty constructor for style issues
+ 	*/
 	protected AccountServerFactory() {
 
 	}
 
+	/** Gets the singleton variable
+	* @return A singleton AccountServerFactory, instantiates a new ASF if singleton is set to null
+	*/
 	public static AccountServerFactory getMe() {
 		if (singleton == null) {
 			singleton = new AccountServerFactory();
@@ -23,6 +28,9 @@ public class AccountServerFactory {
 		return singleton;
 	}
 
+	/** Gets the server solution
+	* @return new server solution object
+	*/
 	public AccountServer lookup() {
 		return new ServerSolution();
 	}
